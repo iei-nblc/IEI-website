2339162bb2716426e46e75476265e2b8b0e90714
Agnisha <2025.cse09.agnishade@gmail.com>
2024-02-09 00:12:53 +0530

    Updated the UI by adding picture in the middle and chairman and secratary nblc on the left side of the picture

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function MainContent() {
   return (
     <main class="container">
-          {
+          {/* {
               <div className="containerBox">
     <div className="textbox">
       <h2>Chairman's View</h2>
@@ -15,7 +15,25 @@ function MainContent() {
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos earum voluptatum officia sint obcaecati ut maxime magnam aut. Non hic id amet, dolore quas explicabo recusandae officiis alias quam magnam qui, expedita esse, ducimus doloribus debitis? Quasi, consectetur fugit vero, reprehenderit unde quod est animi, non facere accusamus asperiores. Fugit molestiae voluptatibus consequuntur illo facilis ea delectus officia obcaecati, nostrum sed totam molestias. Obcaecati amet facilis consequatur et, asperiores officiis voluptas aut! Necessitatibus.</p>
       </div>
       </div>
-              }        
+              }         */
+              
+              
+              <div class="containerBox">
+  <div class="left-column">
+    <div class="upper-text">
+      <h2>Chairman NBLC</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit libero vitae risus tempor, ut commodo lorem ultrices. Nulla facilisi. Sed ac sodales ante.</p>
+    </div>
+    <div class="lower-text">
+      <h2>Secretary NBLC</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit libero vitae risus tempor, ut commodo lorem ultrices. Nulla facilisi. Sed ac sodales ante.</p>
+    </div>
+  </div>
+  <div class="right-column">
+    <img src="image1.jpg" alt="IE NBLC Image" class="image"/>
+  </div>
+</div>
+}
     </main>
   );
 }
