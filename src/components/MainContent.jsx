ad0272875d4ba901c816831d0addc8f9b5de142e
Chinmoy Ghosh <63526141+chinmoy06@users.noreply.github.com>
2024-10-04 16:50:59 +0530

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -12,7 +12,7 @@ function MainContent() {
 
     <div className="textbox">
       <h2>About Us</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos earum voluptatum officia sint obcaecati ut maxime magnam aut. Non hic id amet, dolore quas explicabo recusandae officiis alias quam magnam qui, expedita esse, ducimus doloribus debitis? Quasi, consectetur fugit vero, reprehenderit unde quod est animi, non facere accusamus asperiores. Fugit molestiae voluptatibus consequuntur illo facilis ea delectus officia obcaecati, nostrum sed totam molestias. Obcaecati amet facilis consequatur et, asperiores officiis voluptas aut! Necessitatibus.</p>
+      <p>um sed totam molestias. Obcaecati amet facilis consequatur et, asperiores officiis voluptas aut! Necessitatibus.</p>
       </div>
       </div>
               }         */
